60616e23b2a9ab68e0a3aefd8a96e33e2217e6de
Gabriel Raimondo <46075753+GabrielRai@users.noreply.github.com>
2024-12-03 10:52:46 +0100

    Addede Interface

---
+++ MetaCortex.Products.DataAccess/Interface/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetaCortex.Products.DataAccess.Interface
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Product> GetProduct(string id);
+        Task<Product> CreateProduct(Product product);
+        Task UpdateProduct(string id, Product product);
+        Task DeleteProduct(string id);
+    }
+}
---
+++ MetaCortex.Products.DataAccess/Product.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson;
+using System.ComponentModel;
+
+namespace MetaCortex.Products.DataAccess
+{
+    public class Product
+    {
+        public ObjectId Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
